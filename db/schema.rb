e40ad33013d259562ab9d6e44c07c54a1a1ff2fe
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-04-01 12:58:00 +0300

    Added Submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_150053) do
+ActiveRecord::Schema.define(version: 2020_04_01_093337) do
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

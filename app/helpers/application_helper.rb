9842d6f8b420f9015dc29b11c95dfddcce150910
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-04-01 18:52:34 +0300

    Added gem 'inline_svg' and created a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_svg(name, styles: "fill-current text-gray-400", title:nil)
+    filename = "#{name}.svg"
+    title ||= name.underscore.humanize
+    inline_svg(filename, aria: true, nocomment: true, title: title, class: styles)
+  end
 end

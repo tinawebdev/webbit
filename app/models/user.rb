7f3b6264949580b2c0185e3251646aba29399d2b
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-04-03 13:58:57 +0300

    Added username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :submissions
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_105529) do
+ActiveRecord::Schema.define(version: 2020_04_03_101527) do
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
@@ -32,8 +32,10 @@ ActiveRecord::Schema.define(version: 2020_04_01_105529) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "submissions", "users"

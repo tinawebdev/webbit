6e59d4fafea9922da7270d434327cbc043721825
Kristina Goncharova <kris.goncharova@inbox.ru>
2020-04-04 20:33:38 +0300

    Fixed styles for dropdown menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :find_communities
 
   protected
+
+  def find_communities
+    @communities = Community.all.order(:title)
+  end
 
   def configure_permitted_parameters
     added_attrs = [:username]
--- app/javascript/components/dropdown.js
+++ app/javascript/components/dropdown.js
@@ -1,15 +1,44 @@
 document.addEventListener("turbolinks:load", () => {
-  const dropdown = document.querySelector(".js-dropdown-user-target");
-  const dropdownList = document.querySelector(".dropdown-list");
+  const dropDownLinks = document.querySelectorAll(".js-nav .dropdown a");
+  const dropDownMenu = document.querySelector(".dropdown-menu");
 
-  dropdown.addEventListener("click", (e) => e.preventDefault());
-  document.addEventListener("click", (e) => {
-    if (e.target.closest(".dropdown")) {
-      // hide dropdown if click target is the dropdown element
-      dropdownList.classList.remove("hidden");
-    } else {
-      // remove dropdown if no target is the dropdown element
-      dropdownList.classList.add("hidden");      
+  function dropdown() {
+    dropDownLinks.forEach(function(dropdown) {
+      dropdown.addEventListener("click", (e) => {
+        const dropdownTarget = dropdown.nextElementSibling;
+        // console.log(e.target.nextElementSibling)
+
+        if (e.target.nextElementSibling != null) {
+          // Hide the dropdown
+          dropdownTarget.classList.toggle("hidden");
+          // Click outside of dropdown to also close
+          hideOnClickOutside(dropdown);
+          e.preventDefault();
+        }
+      })
+    });
+  }
+
+  function hideOnClickOutside(element) {
+    const outsideClickListener = function(event) {
+      if (!element.contains(event.target)) {
+        if (!!element && !!(element.offsetWidth || element.offsetHeight || element.getClientRects().length)) {
+          element.nextElementSibling.classList.toggle('hidden');
+          removeClickListener();
+        }
+      }
     }
-  });
+
+    const removeClickListener = function() {
+      document.removeEventListener("click", outsideClickListener);
+    }
+
+    document.addEventListener("click", outsideClickListener);
+  }
+
+  function init() {
+    dropdown();
+  }
+
+  init();
 });

5bb982820c7f3482ff9e26b2e45c2a8c1944187f
Kristina <codingkris@yahoo.com>
2020-04-08 18:28:28 +0300

    Accounting for video, images, and text on submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -12,6 +12,26 @@ class Submission < ApplicationRecord
   validates :title, presence: true
   validates :body, length: { maximum: 8000 }
   validates :url, url: { allow_blank: true }
+  validate :image_or_video
+  validate :url_or_content
 
   acts_as_votable
+
+  private
+
+    def image_or_video
+      unless submission_image.blank? || submission_video.blank?
+        unless submission_image.blank? ^ submission_video.blank?
+          errors.add(:base, "Add an image or video, not both")
+        end
+      end
+    end
+
+    def url_or_content
+      unless url.blank? || body.blank?
+        unless url.blank? ^ body.blank?
+          errors.add(:base, "Add a valid URL or text content")
+        end
+      end
+    end
 end
